5924d92c56ef4d340e5b85010709090d958dc17f
Uwe Koch <uwe.koch@code-it-studio.de>
2022-12-16 04:55:50 +0100

    version bump to 0.2.5

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function main() {
     // parse CLI options
     const program = new Command();
     program
-        .version('0.2.4')
+        .version('0.2.5')
         .option('-p, --port <port>', 'The server will run on this port. By default, any free port is chosen.', 0)
         .option('-c, --content <content>', 'The path that contains the dashboard contents.')
         .option('-o, --open', 'When this is set, the dashboard is opened in the default web browser.')
